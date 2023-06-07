2edb91b62f1c1b832bdd7318e18f5e13f8eb342b
kursor <skurochkin298@gmail.com>
2023-06-07 20:17:13 +0300

    module3-task1 refactoring

--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -1,6 +1,6 @@
-import { destinations } from '../mock/destination.js';
 import {createElement} from '../render.js';
-import { convertToFormDate, capitalizeType, getItemFromItemsById } from '../utils.js';
+import { destinations } from '../mock/destination.js';
+import { convertToBasicime, getItemFromItemsById, capitalizeType } from '../utils.js';
 import { getOffersByType } from '../mock/const.js';
 
 
@@ -9,23 +9,17 @@ function createOffersTemplate(offers, type) {
     const isOfferChecked = offers.includes(offer.id) ? 'checked' : '';
     return `
     <div class="event__offer-selector">
-    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${isOfferChecked}>
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${isOfferChecked}>
       <label class="event__offer-label" for="event-offer-luggage-1">
-      <span class="event__offer-title">${offer.title}</span>
-      &plus;&euro;&nbsp;
-      <span class="event__offer-price">${offer.price}</span>
+        <span class="event__offer-title">${offer.title}</span>
+        &plus;&euro;&nbsp;
+        <span class="event__offer-price">${offer.price}</span>
       </label>
-      </div>`;
+    </div>`;
   }).join('');
 }
 
-function createDestinationPicsTemplate(destination) {
-  return destination.pictures.map((pic) => `
-    <img class="event__photo" src="${pic.src}" alt="${pic.description}">
-  `).join('');
-}
-
-function createFormTemplate(tripPoint) {
+function createEditFormTemplate(tripPoint) {
   const visibility = tripPoint.offersIDs.length === 0 ? 'visually-hidden' : '';
   const destination = getItemFromItemsById(destinations, tripPoint.destination);
   return (
@@ -35,7 +29,7 @@ function createFormTemplate(tripPoint) {
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
           <span class="visually-hidden">Choose event type</span>
-          <img class="event__type-icon" width="17" height="17" src="img/icons/${tripPoint.type}.png" alt="${tripPoint.type} icon">
+          <img class="event__type-icon" width="17" height="17" src="img/icons/${tripPoint.type}.png" alt="${tripPoint.type}">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
         <div class="event__type-list">
@@ -84,7 +78,7 @@ function createFormTemplate(tripPoint) {
         <label class="event__label  event__type-output" for="event-destination-1">
         ${capitalizeType(tripPoint.type)}
         </label>
-        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination.name}" list="destination-list-1">
+<input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination.name}" list="destination-list-1">
         <datalist id="destination-list-1">
           <option value="Amsterdam"></option>
           <option value="Geneva"></option>
@@ -93,43 +87,42 @@ function createFormTemplate(tripPoint) {
       </div>
       <div class="event__field-group  event__field-group--time">
         <label class="visually-hidden" for="event-start-time-1">From</label>
-        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${convertToFormDate(tripPoint.dateFrom)} 00:00">
+        <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${convertToBasicime(tripPoint.dateFrom)}">
         &mdash;
         <label class="visually-hidden" for="event-end-time-1">To</label>
-        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${convertToFormDate(tripPoint.dateFrom)} 00:00">
-        </div>
-        <div class="event__field-group  event__field-group--price">
-          <label class="event__label" for="event-price-1">
-            <span class="visually-hidden">Price</span>
-            &euro;
-          </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
-        </div>
-        <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-        <button class="event__reset-btn" type="reset">Cancel</button>
-      </header>
-      <section class="event__details">
-        <section class="event__section  event__section--offers">
-        <h3 class="event__section-title  event__section-title--offers ${visibility}">Offers</h3>
-        <div class="event__available-offers">
-        ${createOffersTemplate(tripPoint.offersIDs, tripPoint.type)}
-        </div>
+        <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${convertToBasicime(tripPoint.dateTo)}">
+      </div>
+      <div class="event__field-group  event__field-group--price">
+        <label class="event__label" for="event-price-1">
+          <span class="visually-hidden">Price</span>
+          &euro;
+        </label>
+        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="160">
+      </div>
+      <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+      <button class="event__reset-btn" type="reset">Delete</button>
+      <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+      </button>
+    </header>
+    <section class="event__details">
+      <section class="event__section  event__section--offers">
+      <h3 class="event__section-title  event__section-title--offers ${visibility}">Offers</h3>
+      <div class="event__available-offers">
+      ${createOffersTemplate(tripPoint.offersIDs, tripPoint.type)}
+      </div>
       </section>
       <section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
         <p class="event__destination-description">${destination.description}</p>
-        <div class="event__photos-container">
-          <div class="event__photos-tape">
-          ${createDestinationPicsTemplate(destination)}
-          </div>
-        </div>
       </section>
     </section>
   </form>
   </li>`
   );
 }
-export default class CreateFormView {
+
+export default class EditFormView {
   #element = null;
   #tripPoint = null;
 
@@ -138,13 +131,14 @@ export default class CreateFormView {
   }
 
   get template() {
-    return createFormTemplate(this.#tripPoint);
+    return createEditFormTemplate(this.#tripPoint);
   }
 
   get element() {
     if (!this.#element) {
       this.#element = createElement(this.template);
     }
+
     return this.#element;
   }
 
